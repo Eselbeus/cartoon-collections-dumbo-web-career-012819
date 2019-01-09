490d27fe64f17d25ed60e8e0479868530e6f4a58
eselbeus <eselbeus@gmail.com>
2019-01-09 05:15:58 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,12 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(calls)
-  
+  calls.each do |call|
+    if call.length > 4
+      return true 
+    end
+  end
+  return false
 end
 
 def find_the_cheese# code an argument here
